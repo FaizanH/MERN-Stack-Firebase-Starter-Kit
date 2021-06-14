25196b8a270a4ece347c4c4b1839bb3a57c6aa99
Faiz Dev <dev@faiz-dev.tpgi.com.au>
2021-06-14 20:30:02 +1000

    Added axios connection for database

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,26 @@
 const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const ATLAS_URI = "";
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(ATLAS_URI, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+
+const connection = mongoose.connection;
+
+connection.once("open", () => {
+  console.log("MongoDB connection established successfully");
+});
+
+// Add Routes here
+/*
+const exampleRouter = require("./routes/example");
+app.use("/example", exampleRouter);
+*/
+
+exports.app = functions.https.onRequest(app);
--- src/components/example.component.js
+++ src/components/example.component.js
@@ -1,10 +1,35 @@
 import React, { Component } from "react";
 
 export default class Example extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            datalines: []
+        };
+    }
+    componentDidMount() {
+        // Get data from MongoDB
+        /*
+         * axios.get("http://localhost:5001/<project-name>/<region>/app/example")
+            .then(res => {
+                this.setState({
+                    datalines: res.data
+                });
+            })
+            .catch(err => console.log(err));
+        */
+    }
+    
+    datalinesList() {
+        return this.state.datalines.map(currDataLine => {
+            return currDataLine;
+        }
+    }
     
     render() {
         return (
             <div>This is an example Component</div>
+            { this.datalinesList() }
         );
     }
 }
